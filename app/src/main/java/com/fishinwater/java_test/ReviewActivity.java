3f4d62b0b7108133b290d714506266903fd71ae9
FishInWater-1999 <814484626@qq.com>
2019-12-26 17:42:15 +0800

    rebuild project

---
+++ app/src/main/java/com/fishinwater/java_test/ReviewActivity.java
@@ -0,0 +1,25 @@
+package com.fishinwater.java_test;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.fishinwater.fiwview.TextView;
+
+
+/**
+ * 主活动
+ * 显示复习内容
+ *
+ * @author fishinwater-1999
+ */
+public class ReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+
+    }
+}
