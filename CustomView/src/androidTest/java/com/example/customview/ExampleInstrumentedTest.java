c50274022c37686c99b50d087616782010ab58ac
FishInWater-1999 <814484626@qq.com>
2020-02-10 00:18:32 +0800

    textView 布局找不到

--- CustomView/src/androidTest/java/com/example/customview/ExampleInstrumentedTest.java
+++ CustomView/src/androidTest/java/com/example/customview/ExampleInstrumentedTest.java
@@ -17,11 +17,17 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.customview.test", appContext.getPackageName());
+    }
+
+    @Test
+    public void test(){
+        System.out.println(6666);
     }
 }
