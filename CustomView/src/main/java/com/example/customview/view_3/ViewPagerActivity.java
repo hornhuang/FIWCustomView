79fe2cd8432e91839b958c911c0e23d4022b9c6e
FishInWater-1999 <814484626@qq.com>
2020-02-12 14:05:04 +0800

    activity_view_pager.class

---
+++ CustomView/src/main/java/com/example/customview/view_3/ViewPagerActivity.java
@@ -0,0 +1,16 @@
+package com.example.customview.view_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.customview.R;
+
+public class ViewPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+    }
+}
